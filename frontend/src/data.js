cb465b3597e2a4f3949a341173f87d18f06642c7
PUMKIN24 <jer2ymatheww@gmail.com>
2022-08-17 16:13:02 -0700

    react app created and all datas have been changed to dynamic from the erlier template

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const  data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:110,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:110,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:110,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:110,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:110,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:110,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'1',
+            name:'Nike Slim Bomber',
+            category:'Shirts',
+            image:'/images/product-1.jpg',
+            price:0,
+            brand:'Nike',
+            rating:5,
+            numReviews:10,
+            description:'high quality product'
+        }
+         
+    ]
+}
+export default data
